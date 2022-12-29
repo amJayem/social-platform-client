fb5e0e47d2f476c2c2e5f1c5d269807f146eb4f8
amJayem <csejayem19@gmail.com>
2022-12-29 14:48:18 +0600

    user can comment on a post

--- src/Pages/Media/MediaDetails.js
+++ src/Pages/Media/MediaDetails.js
@@ -4,26 +4,37 @@ import { FaThumbsUp } from "react-icons/fa";
 import axios from "axios";
 import useAuthProvider from "../../hooks/useAuthProvider";
 import { toast } from "react-hot-toast";
-import { useQuery } from '@tanstack/react-query';
-import Spinner from "../../hooks/useSpinner";
+import { useQuery } from "@tanstack/react-query";
 
 const MediaDetails = () => {
   const { user } = useAuthProvider();
   const [post, setPost] = useState();
   const [activities, setActivities] = useState();
-  const [react, setReact] = useState(false);
+  const [refresh, setRefresh] = useState(false);
   const { id } = useParams();
 
-  const {data, refetch, isLoading} = useQuery({
-    queryKey: [react],
-    queryFn: async()=>{
+  const { refetch } = useQuery({
+    queryKey: [refresh],
+    queryFn: async () => {
       const res = await fetch(`http://localhost:5000/post-details/${id}`);
       const data = await res.json();
       setPost(data);
       // console.log(data);
       return data;
-    }
-  })
+    },
+  });
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/all-comments/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // setActivities(data);
+      })
+      .catch((e) => {
+        console.error("getting activities error => ", e);
+      });
+  }, [refresh, id]);
 
   useEffect(() => {
     fetch(`http://localhost:5000/activities/${id}`)
@@ -35,7 +46,7 @@ const MediaDetails = () => {
       .catch((e) => {
         console.error("getting activities error => ", e);
       });
-  }, [id,react]);
+  }, [id, refresh]);
 
   const handleAddLike = (value) => {
     const newValue = value + 1;
@@ -48,15 +59,15 @@ const MediaDetails = () => {
         // console.log(data);
 
         if (data.status === 200) {
-            toast.success('You liked this post');
-            refetch()
-            setReact(!react);
+          toast.success("You liked this post");
+          refetch();
+          setRefresh(!refresh);
         }
       })
       .catch((e) => console.error("like error => ", e));
 
-      // reaction storing to db
-      axios
+    // reaction storing to db
+    axios
       .post(`http://localhost:5000/my-reaction`, {
         email: user?.email,
         like: true,
@@ -64,21 +75,35 @@ const MediaDetails = () => {
       })
       .then((data) => {
         console.log(data);
-        setReact(!react);
+        setRefresh(!refresh);
       })
       .then((e) => console.error("storing reaction error => ", e));
   };
 
-  const handlePostComment=(e)=>{
+  const handlePostComment = (e) => {
     e.preventDefault();
     const form = e.target;
     const comment = form.comment.value;
     console.log(comment);
-    
-  }
+    const commentInfo = {
+      email: user?.email,
+      postId: id,
+      comment: comment,
+    };
+    axios
+      .post(`http://localhost:5000/comment`, commentInfo)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => {
+        console.error("commenting error => ", e);
+      });
+
+    setRefresh(!refresh);
+  };
 
   const count = post?.like || 0;
-    // console.log( react);
+  // console.log( react);
   return (
     <div>
       <div className="bg-white shadow-lg rounded-lg p-5">
@@ -96,10 +121,9 @@ const MediaDetails = () => {
             <button
               onClick={() => handleAddLike(count)}
               className="btn btn-primary"
-              disabled={activities?.like === true || react === true}
+              disabled={activities?.like === true || refresh === true}
             >
-              <FaThumbsUp /> 
-              {/* {!react && <Spinner/>} */}
+              <FaThumbsUp />
             </button>
           </div>
           <form onSubmit={handlePostComment} className="py-5 mx-auto">
@@ -112,12 +136,16 @@ const MediaDetails = () => {
                 </div>
               )}
               <textarea
-              name="comment"
+                name="comment"
                 className="textarea textarea-primary w-full"
                 placeholder="Put your comment here"
               ></textarea>
               <div>
-                <input type='submit' value='post' className="btn btn-primary btn-sm "/>
+                <input
+                  type="submit"
+                  value="post"
+                  className="btn btn-primary btn-sm "
+                />
               </div>
             </div>
           </form>
---
+++ src/Pages/Media/postComments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const postComments = () => {
+    return (
+        <div>
+            comments
+        </div>
+    );
+};
+
+export default postComments;
